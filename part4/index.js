dfd9a0f7454b4798711e80865da675b2e55d61e7
John Paul <jpmbelaos@gmail.com>
2026-02-13 16:10:41 +0800

    refactor: move routing to own controllers dir; separate express app and index.js

--- part4/index.js
+++ part4/index.js
@@ -1,28 +1,6 @@
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const express = require('express');
-const mongoose = require('mongoose');
-const Blog = require('./models/blog');
-
-const app = express();
-
-mongoose.connect(config.MONGODB_URI, { family: 4 });
-
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
