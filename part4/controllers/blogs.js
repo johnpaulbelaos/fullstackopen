a00bf238ddbc42859b0ff2f925db970f4acef3fb
John Paul <jpmbelaos@gmail.com>
2026-02-23 14:18:56 +0800

    style: remove trailing space

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -11,7 +11,6 @@ blogsRouter.get('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id).populate('user', { username: 1, name: 1 });
   response.json(blog);
 });
- 
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body;
