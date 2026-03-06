dda3b2c9f9b6202b18df8afa43d93b2a4ce6ba54
John Paul <jpmbelaos@gmail.com>
2026-03-06 12:45:09 +0800

    feat: add users and individual user view

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import {
+  Routes, Route, Link, useNavigate, useMatch
+} from 'react-router-dom'
 
 import Blog from './components/Blog'
 import Login from './components/Login'
@@ -7,19 +10,35 @@ import LoginForm from './components/LoginForm'
 import NewBlog from './components/NewBlog'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
+import Users from './components/Users'
+import User from './components/User'
 
 import { initializeBlogs } from './reducers/blogReducer'
+import { initializeUsers } from './reducers/usersReducer'
 
 const App = () => {
   const dispatch = useDispatch()
-  const blogs = useSelector(({ blog }) => blog)
-  const user = useSelector(({ user }) => user)
-
-  const blogFormRef = useRef()
 
   useEffect(() => {
     dispatch(initializeBlogs())
   }, [dispatch])
+
+  useEffect(() => {
+    dispatch(initializeUsers())
+  }, [dispatch])
+
+  // users store all existing user and is used in conjuction with individualUser to showw individual user view
+  const users = useSelector(({ users }) => users)
+  const match = useMatch('/users/:id')
+  const individualUser = match
+    ? users.find(user => user.id === match.params.id)
+    : null
+  // user stores data of the current logged user
+  const user = useSelector(({ user }) => user)
+
+  const blogs = useSelector(({ blog }) => blog)
+
+  const blogFormRef = useRef()
 
   if (user === null) {
     return (
@@ -41,6 +60,10 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} user={user} />
       )}
+      <Routes>
+        <Route path='/users' element={<Users />} />
+        <Route path='/users/:id' element={<User user={individualUser} />} />
+      </Routes>
     </div>
   )
 }
