fb82a4bf48eab97ae4c5a5e60158f9d521cb9d5f
John Paul <jpmbelaos@gmail.com>
2026-03-11 17:53:35 +0800

    fix: remove reference to unexisting data

--- part8/library-backend/server.js
+++ part8/library-backend/server.js
@@ -18,7 +18,7 @@ const getUserFromAuthHeader = async (auth) => {
   }
  
   const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
-  return User.findById(decodedToken.id).populate('friends')
+  return User.findById(decodedToken.id)
 }
 
   startStandaloneServer(server, {
